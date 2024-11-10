fcaaeb09d683c5280ea7ebb8c61140f81da303a0
negset <15167516+negset@users.noreply.github.com>
2024-11-11 02:46:08 +0900

    create page

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const main = document.querySelector("main");
+  const navHeight = window.getComputedStyle(document.querySelector("nav"))["height"];
+  const footerHeight = window.getComputedStyle(document.querySelector("footer"))["height"];
+  main.style.minHeight = `calc(100vh - ${navHeight} - ${footerHeight})`;
+
+  const navbarBurger = document.querySelector(".navbar-burger");
+  const burgerTarget = document.getElementById(navbarBurger.dataset.target);
+  navbarBurger.onclick = () => {
+    navbarBurger.classList.toggle("is-active");
+    burgerTarget.classList.toggle("is-active");
+  };
+  main.onclick = () => {
+    navbarBurger.classList.remove("is-active");
+    burgerTarget.classList.remove("is-active");
+  };
+});
